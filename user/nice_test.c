dc400479c8215f9106aef00a09faa1a194828dec
anubis-robot <anubis@anubis-lms.io>
2022-11-16 23:16:05 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/nice_test.c
+++ user/nice_test.c
@@ -9,14 +9,37 @@ int main() {
    char **arg = (char *[]){};
    exec(command,arg);
 
-   printf(1,"--Changing the nice value of sh to 5--\n");
+   printf(1,"==Changing the priority of sh to 5==\n");
    char * command1 = "nice";
    char **arg1 = (char *[]){"2","5"};
    exec(command1,arg1);
+   exec(command,arg);
+   
+   printf(1,"==Changing the priority of sh to -5==\n");
+   char * command3 = "nice";
+   char **arg3 = (char *[]){"2","-5"};
+   exec(command3,arg3);
+   exec(command,arg);
 
-   printf(1,"--Changing the nice value of pid = 40--\n");
-    char * command1 = "nice";
-   char **arg1 = (char *[]){"2","5"};
-   exec(command1,arg1);
+   printf(1,"==Changing the priority of sh to 20==\n");
+   char * command4 = "nice";
+   char **arg4 = (char *[]){"2","20"};
+   exec(command4,arg4);
+   exec(command,arg);
+   
+   printf(1,"==Changing the priority of sh to -22==\n");
+   char * command5 = "nice";
+   char **arg5 = (char *[]){"2","-22"};
+   exec(command5,arg5);
+   exec(command,arg);
+
+
+   printf(1,"==Changing the priority of pid = 40==\n");
+   char * command2 = "nice";
+   char **arg2 = (char *[]){"40","5"};
+   exec(command2,arg2);
+   exec(command,arg);
+
+   
    exit();
 }
