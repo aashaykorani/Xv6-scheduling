3c20430475dcca10649438a66683456d820af59a
anubis-robot <anubis@anubis-lms.io>
2022-11-16 21:30:48 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- kernel/exec.c
+++ kernel/exec.c
@@ -90,6 +90,7 @@ int exec(char *path, char **argv) {
   proc->sz = sz;
   proc->tf->eip = elf.entry; // main
   proc->tf->esp = sp;
+  proc->priority = 2;
   switchuvm(proc);
   freevm(oldpgdir);
   return 0;
