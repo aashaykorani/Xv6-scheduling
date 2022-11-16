3a6a99b4110a3cbd5d617f65d438e07f1bf6dbd9
anubis-robot <anubis@anubis-lms.io>
2022-11-16 22:05:53 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/nice.c
+++ user/nice.c
@@ -12,9 +12,17 @@ main(int argc, char *argv[])
     exit();
   }
   pid = atoi(argv[1]);
-  priority = atoi(argv[2]);
-  if argv[2][0] == 
-  printf(1,"This is the atoi %d",priority);
+  // priority = atoi(argv[2]);
+  if (argv[2][0] == '-'){
+    // printf(1,"Yeh %c",argv[2][0]);
+    printf(1,"2nd char = %c",argv[2][1]);
+    priority = atoi(argv[2][1]);
+  }
+  else{
+    priority = atoi(argv[2]);
+    printf(1,"Positive");
+  }
+  // printf(1,"This is the atoi %d",priority);
   if (priority < 0 || priority > 20){
     printf(2,"Invalid priority (0-20)!\n");
     exit();
