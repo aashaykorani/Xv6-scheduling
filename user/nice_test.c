62d9c1413d6d0ea2946c3ee8cfc7be0e88f3c597
anubis-robot <anubis@anubis-lms.io>
2022-11-16 23:58:39 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/nice_test.c
+++ user/nice_test.c
@@ -1,4 +1,3 @@
-// #include "kernel/defs.h"
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user.h"
@@ -17,35 +16,39 @@ void my_system(char *arg[]){
 }
 
 int main() {
-   printf(1,"==Running PS==\n\n"); 
+   printf(1,"==Running PS to show the inital state==\n\n"); 
    static char *arg[]= {"./ps",NULL};
    my_system(arg);
 
    printf(1,"\n==Changing the priority of sh to 5==\n\n");
    static char *arg1[]= {"./nice","2","5",NULL};
    my_system(arg1);
-   printf(1, char *, ...)
+   printf(1,"\n");
    my_system(arg);
    
    printf(1,"\n==Changing the priority of sh to -5==\n\n");
    static char *arg3[]= {"./nice","2","-5",NULL};
    my_system(arg3);
+   printf(1,"\n");
    my_system(arg);
 
    printf(1,"\n==Changing the priority of sh to 20==\n\n");
    static char *arg4[]= {"./nice","2","20",NULL};
    my_system(arg4);
+   printf(1,"\n");
    my_system(arg);
    
    printf(1,"\n==Changing the priority of sh to -22==\n\n");
   static char *arg5[]= {"./nice","2","-22",NULL};
    my_system(arg5);
+   printf(1,"\n");
    my_system(arg);
 
 
    printf(1,"\n==Changing the priority of pid = 40==\n");
    static char *arg2[]= {"./nice","40","5",NULL};
    my_system(arg2);
+   printf(1,"\n");
    my_system(arg);
 
    exit();
