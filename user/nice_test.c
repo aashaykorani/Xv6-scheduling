7db83a5e08704e3871ba644bd4f21b26889cec77
anubis-robot <anubis@anubis-lms.io>
2022-11-16 23:31:08 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/nice_test.c
+++ user/nice_test.c
@@ -1,4 +1,3 @@
-#include "kernel/defs.h"
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user.h"
@@ -6,7 +5,8 @@
 void my_system(char * command,char ** arg){
 
     int pid = fork();
-    if pid
+    if (pid == -1)
+        exit();
     if (pid == 0) // child
         exec(command,arg);
     else
@@ -15,41 +15,41 @@ void my_system(char * command,char ** arg){
 
 int main() {
 
-   printf(1,"==Running PS==\n"); 
+   printf(1,"==Running PS==\n\n"); 
    char * command = "ps";
    char **arg = (char *[]){};
-   exec(command,arg);
+   my_system(command,arg);
 
-   printf(1,"==Changing the priority of sh to 5==\n");
+   printf(1,"==Changing the priority of sh to 5==\n\n");
    char * command1 = "nice";
    char **arg1 = (char *[]){"2","5"};
-   exec(command1,arg1);
-   exec(command,arg);
+   my_system(command1,arg1);
+   my_system(command,arg);
    
-   printf(1,"==Changing the priority of sh to -5==\n");
+   printf(1,"==Changing the priority of sh to -5==\n\n");
    char * command3 = "nice";
    char **arg3 = (char *[]){"2","-5"};
-   exec(command3,arg3);
-   exec(command,arg);
+   my_system(command3,arg3);
+   my_system(command,arg);
 
-   printf(1,"==Changing the priority of sh to 20==\n");
+   printf(1,"==Changing the priority of sh to 20==\n\n");
    char * command4 = "nice";
    char **arg4 = (char *[]){"2","20"};
-   exec(command4,arg4);
-   exec(command,arg);
+   my_system(command4,arg4);
+   my_system(command,arg);
    
-   printf(1,"==Changing the priority of sh to -22==\n");
+   printf(1,"==Changing the priority of sh to -22==\n\n");
    char * command5 = "nice";
    char **arg5 = (char *[]){"2","-22"};
-   exec(command5,arg5);
-   exec(command,arg);
+   my_system(command5,arg5);
+   my_system(command,arg);
 
 
    printf(1,"==Changing the priority of pid = 40==\n");
    char * command2 = "nice";
    char **arg2 = (char *[]){"40","5"};
-   exec(command2,arg2);
-   exec(command,arg);
+   my_system(command2,arg2);
+   my_system(command,arg);
 
    exit();
 }
