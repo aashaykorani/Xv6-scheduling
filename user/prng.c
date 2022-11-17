5c167a8120d7f97399d5c12bfdda32178fac714d
anubis-robot <anubis@anubis-lms.io>
2022-11-17 22:20:17 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/prng.c
+++ user/prng.c
@@ -1,36 +1,53 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user.h"
+#include "stdint.h"
 
-unsigned int x = 548787455, y = 842502087, z = 3579807591, w = 273326509;
 
-unsigned int XORShift() {
-	unsigned int t = x ^ (x << 11);
-	x = y; y = z; z = w;
-	return w = w ^ (w >> 19) ^ t ^ (t >> 8);
-}
 
-int main(){
-    unsigned int values[100];
-    for(int i = 0; i<5;i++){
-        values[i] = XORShift();
-    }
-    // int max,min,mean = 0;
-    for(int i = 0; i<5;i++){
-        printf(1,"%d\n",values[i]);
-        // if (i==0){
-        //     max = values[i];
-        //     min = values[i];
-        //     // mean += values[i];
-        // }
-        // else{
-        //     if (values[i] >= max)
-        //         max = values[i];
-        //     else
-        //         min = values[i];
-        // }
-        // mean += values[i];
-    }
-    // printf(1,"Max = %d, Min = %d, Mean = %d\n",max,min,mean/100);
-    exit();
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// unsigned int x = 548787455, y = 842502087, z = 3579807591, w = 273326509;
+
+// unsigned int XORShift() {
+// 	unsigned int t = x ^ (x << 11);
+// 	x = y; y = z; z = w;
+// 	return w = w ^ (w >> 19) ^ t ^ (t >> 8);
+// }
+
+// int main(){
+//     unsigned int values[100];
+//     for(int i = 0; i<5;i++){
+//         values[i] = XORShift();
+//     }
+//     // int max,min,mean = 0;
+//     for(int i = 0; i<5;i++){
+//         printf(1,"%d\n",values[i]);
+//         // if (i==0){
+//         //     max = values[i];
+//         //     min = values[i];
+//         //     // mean += values[i];
+//         // }
+//         // else{
+//         //     if (values[i] >= max)
+//         //         max = values[i];
+//         //     else
+//         //         min = values[i];
+//         // }
+//         // mean += values[i];
+//     }
+//     // printf(1,"Max = %d, Min = %d, Mean = %d\n",max,min,mean/100);
+//     exit();
+// }
