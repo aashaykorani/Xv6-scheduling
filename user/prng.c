f89857aef4fc16360c89150eb81af4af85c015d8
anubis-robot <anubis@anubis-lms.io>
2022-11-17 23:15:26 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/prng.c
+++ user/prng.c
@@ -130,18 +130,18 @@ long random_at_most(long max) {
 
 
 
-// unsigned long long int xorshift64star(void) {
-//         static unsigned long long int x = 1; /* initial seed must be nonzero, don't use a static variable for the state if multithreaded */
-//         x ^= x >> 12;
-//         x ^= x << 25;
-//         x ^= x >> 27;
-//         return (x * 0x2545F4914F6CDD1DULL);
-// }
+unsigned long long int xorshift64star(void) {
+        static unsigned long long int x = 1; /* initial seed must be nonzero, don't use a static variable for the state if multithreaded */
+        x ^= x >> 12;
+        x ^= x << 25;
+        x ^= x >> 27;
+        return (x * 0x2545F4914F6CDD1DULL);
+}
 
 int main(){
     int values[100];
-    for(int i = 0;i<100;i++){
-        values[i] = random_at_most(100);
+    for(int i = 5;i<100;i++){
+        values[i-5] = random_at_most(i);
     }
     int max,min,mean = 0;
     for(int i = 0; i<100;i++){
@@ -170,17 +170,3 @@ int main(){
 
 
 
-
-
-
-
-
-
-
-// // unsigned int x = 548787455, y = 842502087, z = 3579807591, w = 273326509;
-
-// // unsigned int XORShift() {
-// // 	unsigned int t = x ^ (x << 11);
-// // 	x = y; y = z; z = w;
-// // 	return w = w ^ (w >> 19) ^ t ^ (t >> 8);
-// // }
