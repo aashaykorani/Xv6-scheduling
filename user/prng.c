f677b86281635f6c7d2cf5d7c235e3aeea596211
anubis-robot <anubis@anubis-lms.io>
2022-11-17 22:25:18 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/prng.c
+++ user/prng.c
@@ -1,9 +1,45 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user.h"
-#include "stdint.h"
 
 
+struct xorwow_state {
+    unsigned int x[5];
+    unsigned int counter;
+};
+
+/* The state array must be initialized to not be all zero in the first four words */
+unsigned int xorwow(struct xorwow_state *state)
+{
+    /* Algorithm "xorwow" from p. 5 of Marsaglia, "Xorshift RNGs" */
+    unsigned int t  = state->x[4];
+ 
+    unsigned int s  = state->x[0];  /* Perform a contrived 32-bit shift. */
+    state->x[4] = state->x[3];
+    state->x[3] = state->x[2];
+    state->x[2] = state->x[1];
+    state->x[1] = s;
+ 
+    t ^= t >> 2;
+    t ^= t << 1;
+    t ^= s ^ (s << 4);
+    state->x[0] = t;
+    state->counter += 362437;
+    return t + state->counter;
+}
+
+unsigned long long int xorshift64star(void) {
+    static unsigned long long int x = 1; /* initial seed must be nonzero, don't use a static variable for the state if multithreaded */
+    x ^= x >> 12;
+    x ^= x << 25;
+    x ^= x >> 27;
+    return x * 0x2545F4914F6CDD1DULL;
+}
+
+int main(){
+    printf(1,"%d\n",xorshift64star());
+    exit();
+}
 
 
 
