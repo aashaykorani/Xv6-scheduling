b5764c85ea96a9bc61e023566baa798cef355955
anubis-robot <anubis@anubis-lms.io>
2022-11-18 00:10:31 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/prng.c
+++ user/prng.c
@@ -5,19 +5,15 @@
 #define RAND_MAX 0x7fffffff
 
 unsigned long long int xorshift64star() {
-        static unsigned long long int x = 1; /* initial seed must be nonzero, don't use a static variable for the state if multithreaded */
+        static unsigned long long int x = 1;
         x ^= x >> 12;
         x ^= x << 25;
         x ^= x >> 27;
         return x * 0x2545F4914F6CDD1DULL;
-        // if (q<0)
-        //     return ((q*-1)%max);
-        // return (q%max);
 }
 
 long random(long max){
     unsigned long
-        // max <= RAND_MAX < ULONG_MAX, so this is okay.
         num_bins = (unsigned long) max + 1,
         num_rand = (unsigned long) RAND_MAX + 1,
         bin_size = num_rand / num_bins,
@@ -43,7 +39,6 @@ int main(){
         if (i==0){
             max = values[i];
             min = values[i];
-            // mean += values[i];
         }
         else{
             if (values[i] > max)
