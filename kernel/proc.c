1d8f72907df12bd2545f2cd81cbd56b00ca6477d
anubis-robot <anubis@anubis-lms.io>
2022-11-20 19:14:46 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- kernel/proc.c
+++ kernel/proc.c
@@ -548,3 +548,29 @@ int assign_tickets(int tickets){
   release(&ptable.lock);
   return 0;
 }
+
+#define RAND_MAX 0x7fffffff
+
+unsigned long long int xorshift64star(void) {
+        static unsigned long long int x = 1;
+        x ^= x >> 12;
+        x ^= x << 25;
+        x ^= x >> 27;
+        return x * 0x2545F4914F6CDD1DULL;
+}
+
+long randint(long max){
+    unsigned long
+        num_bins = (unsigned long) max + 1,
+        num_rand = (unsigned long) RAND_MAX + 1,
+        bin_size = num_rand / num_bins,
+        defect   = num_rand % num_bins;
+
+        long x;
+        do {
+        x = xorshift64star();
+        }
+        while (num_rand - defect <= (unsigned long)x);
+
+        return x/bin_size;
+}
