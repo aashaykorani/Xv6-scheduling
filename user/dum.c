9e4fb98cdf5ca2126d5b6fdc59c69218bf10d3ec
anubis-robot <anubis@anubis-lms.io>
2022-11-20 04:16:53 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/dum.c
+++ user/dum.c
@@ -5,33 +5,36 @@
 
 int main(int argc, char *argv[]) {
   // printf(1,"Here = %d\n",assign_tickets(80));
-  int pid;
-  int k, n;
-  int x, z;
-  if(argc < 2)
-	n = 1; //Default
-  else
-	n = atoi(argv[1]);
-  if (n < 0 ||n > 20)
-	n = 2;
-  x = 0;
-  pid = 0;
-
-  for ( k = 0; k < n; k++ ) {
-    pid = fork ();
-    if ( pid < 0 ) {
-      printf(1, "%d failed in fork!\n", getpid());
-    } else if (pid > 0) {
-      // parent
-      printf(1, "Parent %d creating child %d\n",getpid(), pid);
-      wait();
-      }
-      else{
-	printf(1,"Child %d created\n",getpid());
-	for(z = 0; z < 4000000; z+=1)
-	    x = x + 3.14*89.64; //Useless calculation to consume CPU Time
-	break;
-      }
-  }
+  int x;
+  for(int z = 0; z < 4000000; z+=1)
+	    x = x + 3.14*89.64;
+    
   exit();
+  // int pid;
+  // int k, n;
+  // int x, z;
+  // if(argc < 2)
+	// n = 1; //Default
+  // else
+	// n = atoi(argv[1]);
+  // if (n < 0 ||n > 20)
+	// n = 2;
+  // x = 0;
+  // pid = 0;
+
+  // for ( k = 0; k < n; k++ ) {
+  //   pid = fork ();
+  //   if ( pid < 0 ) {
+  //     printf(1, "%d failed in fork!\n", getpid());
+  //   } else if (pid > 0) {
+  //     // parent
+  //     printf(1, "Parent %d creating child %d\n",getpid(), pid);
+  //     wait();
+  //     }
+  //     else{
+	// printf(1,"Child %d created\n",getpid());
+	// for(z = 0; z < 4000000; z+=1)
+	//     x = x + 3.14*89.64; //Useless calculation to consume CPU Time
+	// break;
+  //     }
 }
