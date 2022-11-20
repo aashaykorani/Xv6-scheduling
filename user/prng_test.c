4536bb4c42f94482f2007265e87b04e9f9a636d8
anubis-robot <anubis@anubis-lms.io>
2022-11-20 19:09:45 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/prng_test.c
+++ user/prng_test.c
@@ -7,13 +7,13 @@
 int main(){
     int size = 900;
     int values[size];
-
+    printf(1,"%d",uptime());
     for(int i = 0;i<size;i++){
-        values[i] = random(30000); // Calling random function from the included prng file.
+        values[i] = random(200); // Calling random function from the included prng file.
     }
     int max,min,mean= 0;
     for(int i = 0; i<size;i++){
-        printf(1,"%d, ",values[i]);
+        // printf(1,"%d, ",values[i]);
         if (i==0){
             max = values[i];
             min = values[i];
@@ -26,7 +26,7 @@ int main(){
         }
         mean += values[i];
     }
-    printf(1,"\nMax = %d, Min = %d, Mean = %d\n",max,min,mean/size);
+    // printf(1,"\nMax = %d, Min = %d, Mean = %d\n",max,min,mean/size);
     exit();
 }
 
