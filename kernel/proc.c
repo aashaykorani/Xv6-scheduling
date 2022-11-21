87b0b2009d9b6f1e30c2a79b0a2b167b467029aa
anubis-robot <anubis@anubis-lms.io>
2022-11-21 02:17:25 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- kernel/proc.c
+++ kernel/proc.c
@@ -178,8 +178,6 @@ void exit(void) {
   int fd;
   if (proc == initproc)
     panic("init exiting");
-  // cprintf("Inside exit %s\n",proc->name);
-  // if((strncmp(proc->name,"sh",2)!=0) && (strncmp(proc->name,"init",4)!=0) && (strncmp(proc->name,"",sizeof(proc->name))!=0))
   if(strncmp(proc->name,"dum",3)==0 || strncmp(proc->name,"fork_",5)==0)
     cprintf("Process %s(pid = %d) was CHOSEN TO RUN %d times\n",proc->name,proc->pid,proc->time);
 
