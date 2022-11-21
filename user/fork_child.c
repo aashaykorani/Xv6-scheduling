e05e4d5340d5d04485bacbc5424bacdb5da748f7
anubis-robot <anubis@anubis-lms.io>
2022-11-21 01:47:23 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/fork_child.c
+++ user/fork_child.c
@@ -1,9 +1,10 @@
+// #include "kernel/defs.h"
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user.h"
 #include "kernel/fcntl.h"
 
-void forker(int nprocesses, int parent)
+void forker(int nprocesses, int parent,int tickets)
 {
     int pid;
 
@@ -15,7 +16,8 @@ void forker(int nprocesses, int parent)
         }
         else if (pid == 0)
         {
-            printf(1,)
+            assign_tickets(getpid(),tickets);
+            printf(1,"Parent %d created child %d\n",parent,getpid());
             int x;
             for(int z = 0; z < 400000000; z+=1)
                 x = x + 3.14*89.64;
@@ -23,12 +25,12 @@ void forker(int nprocesses, int parent)
         else if(pid > 0)
         {
             //parent
-            forker(nprocesses - 1,parent);
+            forker(nprocesses - 1,parent,tickets*3);
         }
     }
 }
 
 int main(int argc, char * argv[]){
-    forker(3,getpid());
+    forker(3,getpid(),300);
     exit();
 }
